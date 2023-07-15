26099cb8177a69a886b388f08d9d8757630ed7c1
jay-so <merry9504@naver.com>
2023-07-15 18:27:19 +0900

    feat:Order domain 구현

---
+++ src/main/java/com/example/reactspringbootrestapi/model/Order.java
@@ -0,0 +1,35 @@
+package com.example.reactspringbootrestapi.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Order {
+    private final UUID orderId;
+    private final Email email;
+    private String address;
+    private String postcode;
+    private final List<OrderItem> orderItems;
+    private OrderStatus orderStatus;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public void setAddress(String address) {
+        this.address = address;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
