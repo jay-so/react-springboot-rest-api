67a28f76fdf765bc20f1a676b5713d96ebede951
jay-so <merry9504@naver.com>
2023-07-23 20:26:47 +0900

    feat:ProductServiceImpl 구현

---
+++ src/main/java/com/example/reactspringbootrestapi/service/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.reactspringbootrestapi.service;
+
+import com.example.reactspringbootrestapi.model.Category;
+import com.example.reactspringbootrestapi.model.Product;
+import com.example.reactspringbootrestapi.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public List<Product> getProductsByCategory(Category category) {
+        return productRepository.findByCategory(category);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product createProduct(String productName, Category category, long price) {
+        var product = new Product(UUID.randomUUID(), productName, category, price);
+        return productRepository.insert(product);
+    }
+
+    @Override
+    public Product createProduct(String productName, Category category, long price, String description) {
+        var product = new Product(UUID.randomUUID(), productName, category, price, description, LocalDateTime.now(), LocalDateTime.now());
+        return productRepository.insert(product);
+    }
+}
